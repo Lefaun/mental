feea1bdedf4211d6023f20433eed1e4c173ef430
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-21 18:48:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ st.write(
 
 
 with st.sidebar:
-st.button("Vamos começar!", on_click = clicked, args=[1])
+    st.button("Vamos começar!", on_click = clicked, args=[1])
     if st.button.clicked[1]:
     user_csv = st.file_uploader("Upload your CSV file here", type="csv")
     if user_csv is not None:
