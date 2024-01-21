512ddeccc874a5984a2ba0c28874b3bfa6b78b7d
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-21 13:15:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,49 +20,130 @@ st.write(
 )
 
 
-    tab1, tab2, tab3, tab4 = st.tabs(["The DataFrame","The Maximum Values", "The Minumum Values", "The Average Values"])
-    with tab1:
-        def filter_data(df: pd.DataFrame) ->pd.DataFrame:
-            options = st.multiselect("escolha a Cena ", options=df.columns)
-            st.write('Voçê selecionou as seguintes opções', options)
+tab1, tab2, tab3, tab4 = st.tabs(["The DataFrame","The Maximum Values", "The Minumum Values", "The Average Values"])
+with tab1:
+    def filter_data(df: pd.DataFrame) ->pd.DataFrame:
+        options = st.multiselect("escolha a Cena ", options=df.columns)
+        st.write('Voçê selecionou as seguintes opções', options)
 
-        def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-            """
-            Adds a UI on top of a dataframe to let viewers filter columns
-            Args:
-                df (pd.DataFrame): Original dataframe
-            Returns:
-                pd.DataFrame: Filtered dataframe
-            """
-            modify = st.multiselect(
-            'Fatores de Risco👇',
-            ['Obsidade', 'Diabetes', 'Risco de Saúde', 'Tensão Alta',
-            'Outras Morbilidades', 'Antecedentes familiares'])
+    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Adds a UI on top of a dataframe to let viewers filter columns
+        Args:
+            df (pd.DataFrame): Original dataframe
+        Returns:
+            pd.DataFrame: Filtered dataframe
+        """
+        modify = st.multiselect(
+        'Fatores de Risco👇',
+        ['Obsidade', 'Diabetes', 'Risco de Saúde', 'Tensão Alta',
+        'Outras Morbilidades', 'Antecedentes familiares'])
+
+       # "Escolha os Fatores 👇", df.columns,
+        #label_visibility=st.session_state.visibility,
+        #disabled=st.session_state.disabled,
+        #placeholder=st.session_state.placeholder,
+        if not modify:
+            return df
     
-           # "Escolha os Fatores 👇", df.columns,
+        df = df.copy()
+    
+        # Try to convert datetimes into a standard format (datetime, no timezone)
+        for col in df.columns:
+            if is_object_dtype(df[col]):
+                try:
+                    df[col] = pd.to_datetime(df[col])
+                except Exception:
+                    pass
+    
+            if is_datetime64_any_dtype(df[col]):
+                df[col] = df[col].dt.tz_localize(None)
+    
+        modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            left.write("↳")
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    _min,
+                    _max,
+                    (_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].str.contains(user_text_input)]
+
+    return df        
+
+    def filter_dataframe2(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Adds a UI on top of a dataframe to let viewers filter columns
+        Args:
+            df (pd.DataFrame): Original dataframe
+        Returns:
+            pd.DataFrame: Filtered dataframe
+        """
+        modify2 = st.multiselect(
+            'Fatores de Risco👇',
+        ['Obsidade', 'Diabetes', 'Risco de Saúde', 'Tensão Alta',
+        'Outras Morbilidades', 'Antecedentes familiares'])
             #label_visibility=st.session_state.visibility,
             #disabled=st.session_state.disabled,
             #placeholder=st.session_state.placeholder,
-            if not modify:
-                return df
         
-            df = df.copy()
-        
-            # Try to convert datetimes into a standard format (datetime, no timezone)
-            for col in df.columns:
-                if is_object_dtype(df[col]):
-                    try:
-                        df[col] = pd.to_datetime(df[col])
-                    except Exception:
-                        pass
-        
-                if is_datetime64_any_dtype(df[col]):
-                    df[col] = df[col].dt.tz_localize(None)
-        
-            modification_container = st.container()
-
+    
+        if not modify2:
+            return df
+    
+        df = df.copy()
+    
+        # Try to convert datetimes into a standard format (datetime, no timezone)
+        for col in df.columns:
+            if is_object_dtype(df[col]):
+                try:
+                    df[col] = pd.to_datetime(df[col])
+                except Exception:
+                    pass
+    
+            if is_datetime64_any_dtype(df[col]):
+                df[col] = df[col].dt.tz_localize(None)
+    
+        modification_container = st.container()
+    
         with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+            to_filter_columns = st.multiselect("Selecione os Riscos", df.columns)
             for column in to_filter_columns:
                 left, right = st.columns((1, 20))
                 left.write("↳")
@@ -105,117 +186,36 @@ st.write(
                     if user_text_input:
                         df = df[df[column].str.contains(user_text_input)]
     
-        return df        
+        return df
 
-        def filter_dataframe2(df: pd.DataFrame) -> pd.DataFrame:
-            """
-            Adds a UI on top of a dataframe to let viewers filter columns
-            Args:
-                df (pd.DataFrame): Original dataframe
-            Returns:
-                pd.DataFrame: Filtered dataframe
-            """
-            modify2 = st.multiselect(
-                'Fatores de Risco👇',
-            ['Obsidade', 'Diabetes', 'Risco de Saúde', 'Tensão Alta',
-            'Outras Morbilidades', 'Antecedentes familiares'])
-                #label_visibility=st.session_state.visibility,
-                #disabled=st.session_state.disabled,
-                #placeholder=st.session_state.placeholder,
-            
-        
-            if not modify2:
-                return df
-        
-            df = df.copy()
-        
-            # Try to convert datetimes into a standard format (datetime, no timezone)
-            for col in df.columns:
-                if is_object_dtype(df[col]):
-                    try:
-                        df[col] = pd.to_datetime(df[col])
-                    except Exception:
-                        pass
-        
-                if is_datetime64_any_dtype(df[col]):
-                    df[col] = df[col].dt.tz_localize(None)
-        
-            modification_container = st.container()
-        
-            with modification_container:
-                to_filter_columns = st.multiselect("Selecione os Riscos", df.columns)
-                for column in to_filter_columns:
-                    left, right = st.columns((1, 20))
-                    left.write("↳")
-                    # Treat columns with < 10 unique values as categorical
-                    if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                        user_cat_input = right.multiselect(
-                            f"Values for {column}",
-                            df[column].unique(),
-                            default=list(df[column].unique()),
-                        )
-                        df = df[df[column].isin(user_cat_input)]
-                    elif is_numeric_dtype(df[column]):
-                        _min = float(df[column].min())
-                        _max = float(df[column].max())
-                        step = (_max - _min) / 100
-                        user_num_input = right.slider(
-                            f"Values for {column}",
-                            _min,
-                            _max,
-                            (_min, _max),
-                            step=step,
-                        )
-                        df = df[df[column].between(*user_num_input)]
-                    elif is_datetime64_any_dtype(df[column]):
-                        user_date_input = right.date_input(
-                            f"Values for {column}",
-                            value=(
-                                df[column].min(),
-                                df[column].max(),
-                            ),
-                        )
-                        if len(user_date_input) == 2:
-                            user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                            start_date, end_date = user_date_input
-                            df = df.loc[df[column].between(start_date, end_date)]
-                    else:
-                        user_text_input = right.text_input(
-                            f"Substring or regex in {column}",
-                        )
-                        if user_text_input:
-                            df = df[df[column].str.contains(user_text_input)]
-        
-            return df
     
-        
-    with tab2:
-        st.header("The Maximum Values")
-        st.pd.dataframe("Mentalhealth3.csv")
-        st.title("Data Science for Mental Health")
-        df = pd.Dataframe("MentalHealth3.csv")
+with tab2:
+    st.header("The Maximum Values")
+    st.pd.dataframe("Mentalhealth3.csv")
+    st.title("Data Science for Mental Health")
+    df = pd.Dataframe("MentalHealth3.csv")
+
+with tab3:
+    st.header("The Minumum Values")
+    st.pd.dataframe("Mentalhealth3.csv")
+with tab4:
+    st.header("The Average Values")
+    st.pd.dataframe("Mentalhealth3.csv")
+    st.title("Data Science for Mental Health")
+    df = pd.Dataframe("MentalHealth3.csv")
     
-    with tab3:
-        st.header("The Minumum Values")
-        st.pd.dataframe("Mentalhealth3.csv")
-    with tab4:
-        st.header("The Average Values")
-        st.pd.dataframe("Mentalhealth3.csv")
-        st.title("Data Science for Mental Health")
-        df = pd.Dataframe("MentalHealth3.csv")
-        
-        st.write('Dataframe: Seleção de Colunas')
-        st.dataframe(df)
-        
-        # Add a multiselect widget to select rows based on the index
-        selected_columns = st.multiselect('Select Colums:', df.Columns)
-        
-        # Subset the dataframe with the selected indices
-        selected_columns = df.loc[selected_columns]
-        
-        # Display the selected data
-        st.write('Selected Columns:')
-        st.dataframe(selected_columns)
+    st.write('Dataframe: Seleção de Colunas')
+    st.dataframe(df)
+    
+    # Add a multiselect widget to select rows based on the index
+    selected_columns = st.multiselect('Select Colums:', df.Columns)
+    
+    # Subset the dataframe with the selected indices
+    selected_columns = df.loc[selected_columns]
+    
+    # Display the selected data
+    st.write('Selected Columns:')
+    st.dataframe(selected_columns)
     
         
 def filter_data(df: pd.DataFrame) ->pd.DataFrame:
