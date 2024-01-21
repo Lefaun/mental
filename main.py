daaaac42bc9ae965cdae4a4329d590133779a4af
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-21 13:04:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,33 +20,33 @@ st.write(
 )
 tab1, tab2, tab3 = st.tabs(["Max", "Min", "Mean"])
 
-    with tab1:
-       st.header("The Maximum Values")
-       st.dataframe("Mentalhealth3.csv", width=200)
-       st.title("Data Science for Mental Health")
-       df = pd.Dataframe("MentalHealth3.csv")
-    with tab2:
-       st.header("The Minumum Values")
-       st.dataframe("Mentalhealth3.csv", width=200)
-    with tab3:
-        st.header("The Average Values")
-        st.dataframe("Mentalhealth3.csv", width=200)
-        st.title("Data Science for Mental Health")
-        df = pd.Dataframe("MentalHealth3.csv")
-        
-        st.write('Dataframe: Seleção de Colunas')
-        st.dataframe(df)
-        
-        # Add a multiselect widget to select rows based on the index
-        selected_columns = st.multiselect('Select Colums:', df.Columns)
-        
-        # Subset the dataframe with the selected indices
-        selected_columns = df.loc[selected_columns]
-        
-        # Display the selected data
-        st.write('Selected Columns:')
-        st.dataframe(selected_columns)
-        return df
+with tab1:
+    st.header("The Maximum Values")
+    st.dataframe("Mentalhealth3.csv", width=200)
+    st.title("Data Science for Mental Health")
+    df = pd.Dataframe("MentalHealth3.csv")
+with tab2:
+    st.header("The Minumum Values")
+    st.dataframe("Mentalhealth3.csv", width=200)
+with tab3:
+    st.header("The Average Values")
+    st.dataframe("Mentalhealth3.csv", width=200)
+    st.title("Data Science for Mental Health")
+    df = pd.Dataframe("MentalHealth3.csv")
+    
+    st.write('Dataframe: Seleção de Colunas')
+    st.dataframe(df)
+    
+    # Add a multiselect widget to select rows based on the index
+    selected_columns = st.multiselect('Select Colums:', df.Columns)
+    
+    # Subset the dataframe with the selected indices
+    selected_columns = df.loc[selected_columns]
+    
+    # Display the selected data
+    st.write('Selected Columns:')
+    st.dataframe(selected_columns)
+    return df
         
 def filter_data(df: pd.DataFrame) ->pd.DataFrame:
     options = st.multiselect("escolha a Cena ", options=df.columns)
